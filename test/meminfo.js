616918c58a0bf1f938175b18bd988b40d1fc8012
Thorsten Lorenz <thlorenz@gmx.de>
2014-05-27 17:59:17 +0000

    camel case naming meminfo vars and adding tests

---
+++ test/meminfo.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var test = require('tap').test
+var os = require('os')
+var meminfo = require('../').meminfo
+
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true));
+}
+
+test('\ncalling meminfo without a unit', function (t) {
+  var info = meminfo();  
+  var osi = { totalmem: os.totalmem() }
+
+  t.equal(info.mainTotal, osi.totalmem, 'main total is total memory')
+  t.ok(0 < info.mainBuffers < info.mainTotal, 'non-zero main buffers is less than total')
+  t.ok(0 < info.mainCached < info.mainTotal, 'non-zero main cached is less than total')
+  t.ok(0 < info.mainFree < info.mainTotal, 'non-zero main free is less than total')
+  t.ok(0 < info.mainUsed < info.mainTotal, 'non-zero main used is less than total')
+
+  t.equal(info.lowFree, info.mainFree, 'low free is same as main free')
+  t.equal(info.lowTotal, info.mainTotal, 'low total is same as main total')
+
+  t.ok(0 < info.swapFree < info.swapTotal, 'non zero swap free is less than swap total')
+  t.ok(0 < info.active, 'non zero active')
+
+  t.equal(info.highFree, 0, 'highFree is zero')
+  t.equal(info.highTotal, 0, 'highTotal is zero')
+
+  t.equal(info.inactLaundry, 0, 'inactLaundry is zero')
+  t.equal(info.inactDirty, 0, 'inactDirty is zero')
+  t.equal(info.inactClean, 0, 'inactClean is zero')
+  t.equal(info.inactTarget, 0, 'inactTarget is zero')
+  ;
+
+  // not too sure about these, they are all 0 on my machine
+  [ 'swapCached', 'swapUsed', 'writeback', 'reversemaps', 'dirty'
+  ].forEach(function (k) { 
+      t.ok(isFinite(info[k]), 'includes ' + k)
+    });
+
+  [ 'slab', 'committedAs', 'inactive', 'mapped', 'pagetables'
+  ].forEach(function (k) { 
+      t.ok(0 < info[k], 'includes non zero ' + k)
+    });
+
+  t.end()
+})
+
+test('\ncalling meminfo with unit k', function (t) {
+  var info = meminfo();
+  var infok = meminfo('k');  
+  t.equal(infok.mainTotal, Math.floor(info.mainTotal / Math.pow(2, 10)), 'main total is returned in kb')
+  t.equal(infok.mainFree, Math.floor(info.mainFree / Math.pow(2, 10)), 'main free is returned in kb')
+  t.end()
+})
+
+test('\ncalling meminfo with unit m', function (t) {
+  var info = meminfo();
+  var infom = meminfo('m');  
+  t.equal(infom.mainTotal, Math.floor(info.mainTotal / Math.pow(2, 20)), 'main total is returned in mb')
+  t.equal(infom.mainFree, Math.floor(info.mainFree / Math.pow(2, 20)), 'main free is returned in mb')
+  t.end()
+})
